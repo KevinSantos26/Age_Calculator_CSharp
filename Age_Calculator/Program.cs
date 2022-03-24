aad46d18f229a51b2e51b795f86d9cc478275134
zCodexs <zCodexs@DESKTOP-L0ENJQF>
2022-03-24 11:00:51 -0300

    Add project files.

---
+++ Age_Calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a, b, c;
+
+
+
+            Console.Write("Digite o ano atual: ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Coloque seu ano de nascimento: ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            c = a - b;
+            Console.WriteLine("Sua idade é: " + c);
+
+            Console.ReadKey();
+
+        }
+    }
+}
